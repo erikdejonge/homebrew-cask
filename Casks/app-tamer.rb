c5032a21f315a442e5a56994652d607b2eee68c4
Erik de Jonge <erik@a8.nl>
2019-06-27 09:07:35 +0200

    Update app-tamer.rb
    sha256 is not the correct one

--- Casks/app-tamer.rb
+++ Casks/app-tamer.rb
@@ -1,6 +1,6 @@
 cask 'app-tamer' do
   version '2.4.6'
-  sha256 '7e4333a3661c20c64fcb1b982ced17228e094253eaaf5d843fc1f7813d919098'
+  sha256 'e2b7c5da22be199ec4e54aa2242721fdfe86d781646358f61b758f3a7030d9a6'
 
   url "https://www.stclairsoft.com/download/AppTamer-#{version}.dmg"
   appcast 'https://www.stclairsoft.com/cgi-bin/sparkle.cgi?AT'
